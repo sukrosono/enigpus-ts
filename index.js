aa730a9f0d61899574a8370360c9f45abb6c1c00
672019710 <672019710@student.uksw.edu>
2022-11-08 22:20:10 +0700

    chore: add data sample

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ book= new Book('x014', 'Head first spring', 'Enigma', 2012, 'Jution');
 library.add(book);
 book= new Book('x015', 'Head first PHP', 'ap programming', 2013, 'Adi');
 library.add(book);
+book= new Book('x016', 'Head first CSS', 'Enigma', 2014, 'Jution');
+library.add(book);
+book= new Book('x017', 'Head first HTML', 'Enigma', 2015, 'Jution');
+library.add(book);
 
 console.log(library.searchByTitle('Head first PHP'));
 console.log(library.getAll());
